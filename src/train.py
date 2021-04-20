292c4eaa60356d2caf6635a7be228a37c4a9d59a
Srijarko Roy <66861243+srijarkoroy@users.noreply.github.com>
2021-04-20 10:44:28 +0530

    fixed train.py

--- src/train.py
+++ src/train.py
@@ -2,7 +2,7 @@ from tensorflow.keras.losses import binary_crossentropy
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.metrics import Accuracy
 
-def train(model, training_set, validation_set, batch_size = None, epochs, verbose = 1):
+def train(model, model_name, training_set, validation_set, batch_size, epochs, verbose = 1):
     
     ''' The 'model' will be trained using the Adam optimizer, with binary_crossentropy loss and accuracy as the metric on the 'training_set' using 'validation_set' for validation with the mentioned 'batch_size' for 'epoch' number of times '''
 
@@ -10,4 +10,4 @@ def train(model, training_set, validation_set, batch_size = None, epochs, verbos
     
     model.fit(training_set, validation_data = validation_set, batch_size = batch_size, epochs = epochs, verbose = verbose)
 
-    model.save('./models/model.h5')
+    model.save('./models/'+model_name+'.h5')
